9bfba401afc47a90e9f7b3ff3c55d27139f99c4c
dingzhengru <qws7869vdx@gmail.com>
2020-05-04 16:58:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+<div id="app">
+  <h1>CSS3 轉盤抽獎</h1>
+  <code>來自: https://mirari.cc/2019/06/25/CSS3实现转盘抽奖</code>
+  
+  <SpinWheel 
+    :prizeList="prizeList"
+    :time="5"
+  />
+
+</div>
+</template>
+
+<script>
+
+import SpinWheel from '@/components/SpinWheel'
+
+export default {
+  name: 'App',
+  components: {
+    SpinWheel
+  },
+  data() {
+    return {
+      prizeList: [
+        {
+          icon: "https://picsum.photos/40?random=1",
+          name: "$10000"
+        },
+        {
+          icon: "https://picsum.photos/40?random=6",
+          name: "Thank you!"
+        },
+        {
+          icon: "https://picsum.photos/40?random=2",
+          name: "$500"
+        },
+        {
+          icon: "https://picsum.photos/40?random=3",
+          name: "$100"
+        },
+        {
+          icon: "https://picsum.photos/40?random=6",
+          name: "Thank you!"
+        },
+        {
+          icon: "https://picsum.photos/40?random=4",
+          name: "$50"
+        },
+        {
+          icon: "https://picsum.photos/40?random=5",
+          name: "$10"
+        },
+        {
+          icon: "https://picsum.photos/40?random=6",
+          name: "Thank you!"
+        },
+      ]
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
---
+++ src/components/SpinWheel.vue
@@ -0,0 +1,184 @@
+<template>
+<div id="spin-wheel">
+  <div class="wheel-wrapper">
+    <div class="wheel-pointer" @click="onClickRotate">
+      Start
+    </div>
+    <div class="wheel-bg" 
+      :class="{freeze: freeze}" 
+      :style="{ 'transform': `rotate(${wheelDeg}deg)`,
+                'transition': `transform ${time}s ease-in-out`}"
+    >
+      <div class="prize-list">
+        <div class="prize-item-wrapper" v-for="(item,index) in prizeList" :key="index">
+          <div class="prize-item" :style="`transform: rotate(${(360 / prizeList.length) * index}deg)`">
+            <div class="prize-name">
+              {{ item.name }}
+            </div>
+            <div class="prize-icon">
+              <img :src="item.icon">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: 'SpinWheel',
+  props: {
+    prizeList: {
+      type: Array,
+      default: () => []
+    },
+    time: { // 旋轉時間
+      type: Number,
+      default: 4
+    },
+  },
+  data() {
+    return {
+      freeze: false,
+      rolling: false,
+      wheelDeg: 0,
+    }
+  },
+  computed: {
+    // prizeList() {
+    //   return this.prizeList.slice(0, this.prizeNumber)
+    // }
+  },
+  methods: {
+    onClickRotate() {
+      if (this.rolling) {
+        return
+      }
+
+      // result: 假設有 8 個獎項 => 0 ~ 8
+      // 0~1 代表中第一個 1~2 代表中第二個...以此類推
+      const result = Math.floor(Math.random() * this.prizeList.length)
+      this.roll(result)
+    },
+    roll(result) {
+      this.rolling = true
+
+      const { wheelDeg, prizeList } = this
+
+      this.wheelDeg =
+        wheelDeg -
+        wheelDeg % 360 +
+        (6 + this.time) * 360 +
+        (360 - 360 / prizeList.length * result)
+
+      setTimeout(() => {
+        this.rolling = false
+        alert("Result：" + prizeList[result].name)
+      }, this.time * 1000 + 500)
+    }
+  },
+  // watch: {
+  //   prizeNumber() {
+  //     this.freeze = true
+  //     this.wheelDeg = 0
+
+  //     setTimeout(() => {
+  //       this.freeze = false
+  //     }, 0)
+  //   }
+  // }
+}
+</script>
+
+<style lang="scss">
+
+
+$wheelWidth: 300px;
+$wheelHeight: 300px;
+
+$prizeWidth: $wheelWidth / 2;
+$prizeHeight: $wheelHeight / 2;
+
+.wheel-wrapper {
+  width: $wheelWidth;
+  height: $wheelHeight;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.wheel-pointer {
+  width: 60px;
+  height: 60px;
+  border-radius: 1000px;
+
+  background: yellow;
+
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+  line-height: 60px;
+  z-index: 10;
+  cursor: pointer;
+
+  &::after {
+    content: "";
+    position: absolute;
+    top: -32px;
+    left: 50%;
+    border-width: 0 8px 40px;
+    border-style: solid;
+    border-color: transparent transparent yellow;
+    transform: translateX(-50%);
+  }
+}
+.wheel-bg {
+  width: 100%;
+  height: 100%;
+  border-radius: 1000px;
+  overflow: hidden;
+  // transition: transform $transitionTime ease-in-out;
+
+  background: #7eef97;
+
+  &.freeze {
+    transition: none;
+    background: red;
+  }
+}
+
+.prize-list {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  text-align: center;
+}
+
+.prize-item-wrapper {
+  width: $prizeWidth;
+  height: $prizeHeight;
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.prize-item {
+  width: 100%;
+  height: 100%;
+  transform-origin: bottom;
+
+  .prize-name {
+    padding: 16px 0;
+  }
+
+  .prize-icon {
+  }
+}
+</style>
